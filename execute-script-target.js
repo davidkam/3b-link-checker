8b86aa6fa47eca6db60c22ad4bd60b7d78164cf8
David Kam <davekam@davekam.com>
2020-05-14 11:23:35 -0500

    added resource type field to report

--- execute-script-target.js
+++ execute-script-target.js
@@ -1,59 +1,79 @@
 var data = JSON.parse(document.body.textContent);
 var baddata = [];
 var gooddata = [];
-var checkdata = [];
+var alldata = new Map();
 data.forEach((resource) => {
-  resource.resourceLanguages.forEach((resourceLanguage) => {
-    id = resourceLanguage.resourceId;
-    link = resourceLanguage.resourceLink;
-    name = resourceLanguage.resourceName;
-    language = resourceLanguage.language;
-    checkdata.push({'id': id, 'link': link, 'name': name, 'language': language});
-  });
+    resource.resourceLanguages.forEach((resourceLanguage) => {
+        alldata.set(resourceLanguage.resourceId, {
+            'id': resourceLanguage.resourceId,
+            'link': resourceLanguage.resourceLink,
+            'name': resourceLanguage.resourceName,
+            'resource_type': resourceLanguage.resourceType,
+            'language': resourceLanguage.language,
+            'owner': resourceLanguage.resourceContactEmail,
+            'created': resourceLanguage.createdAt,
+            'updated': resourceLanguage.updatedAt
+        });
+    });
 });
 let requests = [];
-checkdata.forEach((resource) => {
-  if(resource.link != '') {
-    requests.push(checklink(resource));
-  } else {
-    baddata.push({'id': resource.id, 'link': resource.link, 'name': resource.name, 'language': resource.language, 'msg': 'empty link'});
-  }
+alldata.forEach(function(resource, resourceId) {
+    if (resource.link != '') {
+        requests.push(checklink(resource));
+    } else {
+        baddata.push({
+            'id': resourceId,
+            'msg': 'empty link'
+        });
+    }
 });
-
 Promise.all(requests).then(allfinished);
 
 function allfinished() {
-  let csvContent = "data:text/csv;charset=utf-8,";
-  csvContent += '"Resource Name","Resource Link","Link","Status message"' + '\\u000A';
-  gooddata.forEach(function(record) {
-    csvContent += makecsv(record, 'GOOD');
-  });
-  baddata.forEach(function(record) {
-    csvContent += makecsv(record, 'ERROR');
-  });
-  var encodedUri = encodeURI(csvContent);
-  console.log(csvContent);
-  window.open(encodedUri);
+    let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += '"Status Message","Resource Name","Resource Type","Owner","Created At","Last Modified","Resource Link","Link"' + '\\\\u000A';
+    gooddata.forEach(function(record) {
+        csvContent += makecsv(record, 'GOOD');
+    });
+    baddata.forEach(function(record) {
+        csvContent += makecsv(record, 'ERROR');
+    });
+    var encodedUri = encodeURI(csvContent);
+    console.log(csvContent);
+    window.open(encodedUri);
 }
 
 function makecsv(record, msgprefix) {
-  line = '"' + record.name.replace(/"/g, '""') + '",';
-  line += '"' + record.link.replace(/"/g, '""').replace(/#/g, '%23') + '",';
-  line += '"' + window.location.origin + '/' + record.language + '/resources?resourceId=' + record.id.replace(/"/g, '""') + '",';
-  line += '"' + msgprefix + ': ' + record.msg.toString().replace(/"/g, '""') + '"' + '\\u000A';
-  return line;
+    id = record.id;
+    resource = alldata.get(id);
+    line = '"' + msgprefix + ': ' + record.msg.toString().replace(/"/g, '""') + '",';
+    line += '"' + resource.name.replace(/"/g, '""') + '",';
+    line += '"' + resource.resource_type.replace(/"/g, '""') + '",';
+    line += '"' + resource.owner.replace(/"/g, '""') + '",';
+    line += '"' + resource.created.replace(/"/g, '""') + '",';
+    line += '"' + resource.updated.replace(/"/g, '""') + '",';
+    line += '"' + resource.link.replace(/"/g, '""').replace(/#/g, '%23') + '",';
+    line += '"' + window.location.origin + '/' + resource.language + '/resources?resourceId=' + resource.id.replace(/"/g, '""') + '"' + '\\\\u000A';
+    return line;
 }
 
 function checklink(resource) {
-  return fetch(resource.link).then(
-    function(response) {
-      if(response.status >= 200 && response.status <= 299) {
-        gooddata.push({'id': resource.id, 'link': resource.link, 'name': resource.name, 'language': resource.language, 'msg': response.status});
-      } else {
-        baddata.push({'id': resource.id, 'link': resource.link, 'name': resource.name, 'language': resource.language, 'msg': response.status});
-      }
-    }
-  ).catch(function(err) {
-    baddata.push({'id': resource.id, 'link': resource.link, 'name': resource.name, 'language': resource.language, 'msg': err});
-  });
+    return fetch(resource.link).then(function(response) {
+        if (response.status >= 200 && response.status <= 299) {
+            gooddata.push({
+                'id': resource.id,
+                'msg': response.status
+            });
+        } else {
+            baddata.push({
+                'id': resource.id,
+                'msg': response.status
+            });
+        }
+    }).catch(function(err) {
+        baddata.push({
+            'id': resource.id,
+            'msg': err
+        });
+    });
 }
